df0975ff6bf4a3c166c02aafcc0ff7934923ed34
Execotryx <execotryx@gmail.com>
2017-07-21 17:36:33 +0600

    Project 5 - JQuery Badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,24 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax(
+    {
+      method: 'get',
+      url: 'https://www.codeschool.com/users/1250747.json',
+      dataType: 'jsonp',
+      success: function(response) {
+          let completedCourses = response.courses.completed;
+          let badges = $('#badges');
+          $.each(completedCourses, function(index, course) {
+            $('<div/>').addClass('course')
+                       .append($('<h3/>').text(course.title))
+                       .append($('<img/>').prop('src', course.badge))
+                       .append($('<a/>').prop('href', course.url)
+                                        .prop('target', '_blank')
+                                        .text('See Course')
+                                        .addClass('btn btn-primary'))
+                       .appendTo(badges);
+          });
+      }
+    });
 });
